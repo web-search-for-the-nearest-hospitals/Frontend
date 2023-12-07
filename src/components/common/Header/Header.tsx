91c59017663ecd3c9d56e3ae56a4490e5331eda5
Apple <IIKolchin@yandex.ru>
2023-12-08 00:06:38 +0200

    add common components: header, map, dropdown

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,28 +1,13 @@
-import { NavLink } from 'react-router-dom';
 import './Header.scss';
+
+import logo from '../../../assets/icons/logo.svg';
+import user from '../../../assets/icons/user.svg';
 
 function Header() {
   return (
     <header className="header">
-      <h1 className="header__title">Я просто сайт</h1>
-      <nav className="header__link-list">
-        <NavLink
-          className={({ isActive, isPending }) =>
-            isPending ? 'pending' : isActive ? 'header__link header__link_active' : 'header__link'
-          }
-          to={'/clinic-searcher/main'}
-        >
-          Главная
-        </NavLink>
-        <NavLink
-          className={({ isActive, isPending }) =>
-            isPending ? 'pending' : isActive ? 'header__link header__link_active' : 'header__link'
-          }
-          to={'/clinic-searcher/map'}
-        >
-          Карта
-        </NavLink>
-      </nav>
+      <img className="header__logo" src={logo} alt="Логотип" />
+      <img className="header__user" src={user} alt="Иконка пользователя" />
     </header>
   );
 }
