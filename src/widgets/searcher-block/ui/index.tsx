6175ffedf6045ec1a12c9e53127b80d8cbf81df5
Vera Baldina~ <vera.b97@mail.ru>
2023-12-30 02:12:38 +0300

    refactor: отрефакторить код в блоке поиска

--- src/widgets/searcher-block/ui/index.tsx
+++ src/widgets/searcher-block/ui/index.tsx
@@ -1,24 +1,25 @@
 import './index.scss';
 import { useState } from 'react';
-import SubmitButton from '~/shared/ui/SubmitButton/SubmitButton';
-import SearchInput from '~/shared/ui/SearchInput/SearchInput';
-import Checkbox from '~/shared/ui/Checkbox/Checkbox';
-import { specialties } from '../lib/consts';
+
+import { Button, DropDownMenu, Checkbox } from '~/shared/ui/index';
+
+const specialties = ['Терапевт', 'Офтальмолог', 'Хирург', 'Отоларинголог'];
+// @TODO: заменить на данные с бэка
 
 export default function Searcher() {
   const [specialty, setSpeciality] = useState(specialties[0]!);
-  const [allDay, setAllDay] = useState(false);
-  const [civil, setCivil] = useState(false);
+  const [isWorkAllDay, setIsWorkAllDay] = useState(false);
+  const [isGovernment, setIsGovernment] = useState(false);
 
   return (
     <div className="search-clinic">
       <div className="search-clinic__container">
-        <SearchInput values={specialties} state={specialty} setState={setSpeciality}></SearchInput>
-        <SubmitButton value={specialty} title="Найти" />
+        <DropDownMenu values={specialties} placeholder="Врач, специальность" setState={setSpeciality}></DropDownMenu>
+        <Button value={specialty} type="submit" size="s" title="Найти" />
       </div>
       <div className="search-clinic__group">
-        <Checkbox state={allDay} setState={setAllDay} title="Круглосуточные"></Checkbox>
-        <Checkbox state={civil} setState={setCivil} title="Государственные"></Checkbox>
+        <Checkbox state={isWorkAllDay} setState={setIsWorkAllDay} title="Круглосуточные"></Checkbox>
+        <Checkbox state={isGovernment} setState={setIsGovernment} title="Государственные"></Checkbox>
       </div>
     </div>
   );
