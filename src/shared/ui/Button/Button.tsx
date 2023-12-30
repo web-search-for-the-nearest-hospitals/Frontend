ae1f900ea7cc58d238c69fea5538dfe716817d83
Vera <121804229+verabald@users.noreply.github.com>
2023-12-30 08:02:01 +0300

    feat/search (#47)
    * feat: добавлены элементы в searcher-block
    
    * refactor: изменен отступ после обновления ветки
    
    * refactor: отрефакторить код в блоке поиска
    
    * refactor: избавиться от лишнего

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,25 @@
+import cn from 'classnames';
+
+import styles from './Button.module.scss';
+
+interface IButton {
+  onClick?: () => void;
+  title: string;
+  value: string;
+  type: 'submit' | 'button';
+  size: 's' | 'l';
+  disabled?: boolean;
+}
+
+export default function Button({ title, type = 'submit', size = 's', disabled, onClick }: IButton) {
+  return (
+    <button
+      type={type}
+      className={cn(styles['submit-button'], styles[`submit-button_size_${size}`])}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {title}
+    </button>
+  );
+}
