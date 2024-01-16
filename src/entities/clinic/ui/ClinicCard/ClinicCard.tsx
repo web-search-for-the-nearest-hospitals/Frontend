4b3a8ce507c2561ed717dae9cffd32e1a744413d
Vera <121804229+verabald@users.noreply.github.com>
2024-01-16 04:28:09 +0300

    feat/clinic popup (#55)
    * refactor(main): вынес стили для главной страницы, добавил примерный адаптив под mobile
    
    * feat(fullcardclinic): сверстать схематично попап клиники
    
    * refactor(entities): перенес advert-list & clinic-list в ui сегменты соотв-их срезов слоя entities
    
    * refactor: допилил mobile, теперь как надо выглядит, но пришлось стили map__group затронуть
    
    * refactor: перенес содержиоме cardlist в main, исправил адаптив, использовал миксины
    
    * refactor: вынести попап в ui
    
    * feat: создать контейнер для попапа
    
    * feat: добавить кнопку закрытия
    
    * style: внести правки в стиль
    
    * refactor: исправить стилизация попапа
    
    * refactor: переименовать компонент
    
    * style: добавить адаптив без мобильной страницы
    
    * style: исправить вёрстку под макет и убрать конфликты с develop
    
    * style: вёрстка мобильной версии попапа
    
    * feat: добавить открытие попапа(без функции закрытия)
    
    * feat: добавить закрытие попапа

--- src/entities/clinic/ui/ClinicCard/ClinicCard.tsx
+++ src/entities/clinic/ui/ClinicCard/ClinicCard.tsx
@@ -3,12 +3,13 @@ import { IOrganization } from '~/shared/lib/types/interfaces';
 
 interface IClinicCard {
   clinic: IOrganization;
+  isOpenCard: (newVal: boolean) => void;
 }
 
 // TODO: написать парсер для business_hours в формат макета - поле с графиком работы
-export function ClinicCard({ clinic }: IClinicCard) {
+export function ClinicCard({ clinic, isOpenCard }: IClinicCard) {
   return (
-    <div className="clinic-card" onClick={() => alert('Представьте, что я Попап! Большой и информативный!')}>
+    <div className="clinic-card" onClick={() => isOpenCard(true)}>
       <h3 className="clinic-card__name">{clinic.short_name}</h3>
       <div className="clinic-card__timetable">
         <p className="clinic-card__timetable-title">График работы:</p>
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,8 +1,10 @@
 import Button from './Button/Button';
 import ToggleButton from './ToggleButton/ToggleButton';
+import CloseButton from './CloseButton/CloseButton';
 import Checkbox from './Checkbox/Checkbox';
 import DropDownInput from './DropDownInput/DropDownInput';
 import Layout from './Layout/Layout';
+import Popup from './Popup/Popup';
 import Maps from './Maps';
 
-export { Button, ToggleButton, Checkbox, Layout, Maps, DropDownInput };
+export { Button, ToggleButton, CloseButton, Checkbox, Layout, Maps, DropDownInput, Popup };
