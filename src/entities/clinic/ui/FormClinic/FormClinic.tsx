1a3ab8ff86336208fcfdf33262626ecb04ab5201
Magomed Musakadiev <musakadiev-mag@yandex.ru>
2024-01-19 14:34:25 +0300

    fix(entities/clinic/ui/formclinic and pages/reg-clinic/ui): исправления после ревью Веры

--- src/entities/clinic/ui/FormClinic/FormClinic.tsx
+++ src/entities/clinic/ui/FormClinic/FormClinic.tsx
@@ -4,28 +4,34 @@ import { Button } from '~/shared/ui/index';
 function FormClinic() {
   return (
     <>
-      <form className="form-clinic">
-        <div className="form-clinic__container">
-          <h1 className="form-clinic__title">Регистрация клиники</h1>
-          <section className="form-clinic__progress-scale">
-            <article className="form-clinic__progress-element ">
-              <div className="form-clinic__progress-element-circle active-circle-and-line">1</div>
-              <div className="form-clinic__progress-element-line active-circle-and-line"></div>
-              <p className="form-clinic__progress-element-text active-text">Информация о клинике</p>
-            </article>
-            <article className="form-clinic__progress-element">
-              <div className="form-clinic__progress-element-circle ">2</div>
-              <div className="form-clinic__progress-element-line"></div>
-              <p className="form-clinic__progress-element-text ">Информация о специалистах</p>
-            </article>
-            <article className="form-clinic__progress-element">
-              <div className="form-clinic__progress-element-circle ">3</div>
-              <p className="form-clinic__progress-element-text ">Авторизация</p>
-            </article>
-          </section>
-          <Button title={'Далее'} type={'submit'} size={'m'} disabled={true} />
-        </div>
-      </form>
+      <main>
+        <form className="form-clinic">
+          <div className="form-clinic__container">
+            <h1 className="form-clinic__title">Регистрация клиники</h1>
+            <section className="form-clinic__progress-scale">
+              <div className="form-clinic__progress-element ">
+                <div className="form-clinic__progress-element-circle form-clinic__progress-element-circle_active">
+                  1
+                </div>
+                <div className="form-clinic__progress-element-line form-clinic__progress-element-line_active"></div>
+                <p className="form-clinic__progress-element-text form-clinic__progress-element-text_active">
+                  Информация о клинике
+                </p>
+              </div>
+              <div className="form-clinic__progress-element">
+                <div className="form-clinic__progress-element-circle ">2</div>
+                <div className="form-clinic__progress-element-line"></div>
+                <p className="form-clinic__progress-element-text ">Информация о специалистах</p>
+              </div>
+              <div className="form-clinic__progress-element">
+                <div className="form-clinic__progress-element-circle ">3</div>
+                <p className="form-clinic__progress-element-text ">Авторизация</p>
+              </div>
+            </section>
+            <Button title={'Далее'} type={'submit'} size={'m'} disabled={true} />
+          </div>
+        </form>
+      </main>
     </>
   );
 }
