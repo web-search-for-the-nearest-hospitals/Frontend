0771f4d3d93f0a5a87094e4981a1a8b83e0c3f4a
DariaBold <dafkajh@gmail.com>
2024-01-21 02:56:10 +0300

    fix(popup): при первой загрузке страницы не существовал компонент popup
    Немного поправлен код. Теперь компонент существует, а null присваивается FullCardClinic

--- src/pages/main/ui/index.tsx
+++ src/pages/main/ui/index.tsx
@@ -39,11 +39,9 @@ export default function MainPage() {
         <Searcher onClick={triggerQuery} />
         <MapBlock clinicData={data} handleCardClick={handleCardClick} />
       </div>
-      {selectedCard ? (
-        <Popup isOpen={isOpen} closePopup={() => setIsOpen(false)}>
-          <FullCardClinic isClose={() => setIsOpen(false)} clinic={selectedCard} />
-        </Popup>
-      ) : null}
+      <Popup isOpen={isOpen} closePopup={() => setIsOpen(false)}>
+        {selectedCard ? <FullCardClinic isClose={() => setIsOpen(false)} clinic={selectedCard} /> : null}
+      </Popup>
     </div>
   );
 }
