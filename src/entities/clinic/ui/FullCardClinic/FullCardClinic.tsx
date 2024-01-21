7846d2ff70ac3fa706917d1bf4fcfe97f740aaa0
DariaBold <dafkajh@gmail.com>
2024-01-21 22:25:31 +0300

    fix(fullcardclinic): не всегда отрабатывалось определение мобильной версии, переписано через width
    Теперь все устройства, ширина которых меньше 625px, определяются как мобильные для звонков

--- src/entities/clinic/ui/FullCardClinic/FullCardClinic.tsx
+++ src/entities/clinic/ui/FullCardClinic/FullCardClinic.tsx
@@ -8,10 +8,9 @@ interface IFullCard {
 }
 
 export function FullCardClinic({ isClose, clinic }: IFullCard) {
-  function getDevice() {
-    const userAgent = navigator.userAgent.toLowerCase();
-    const isPhone = /mobile|iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(userAgent);
-    if (isPhone) {
+  function getIsPhone() {
+    const width = window.screen.width;
+    if (width < 625) {
       return true;
     } else {
       return false;
@@ -37,13 +36,13 @@ export function FullCardClinic({ isClose, clinic }: IFullCard) {
       </div>
       <div className="clinic-popup__phone">
         <p className="clinic-popup__phone-title">Телефон: </p>
-        {getDevice() ? (
-          <a className="clinic-popup__phone-number clinic-popup__phone-number_active" href={`tel:${clinic.phone}`}>
-            {clinic.phone}
-          </a>
-        ) : (
-          <p className="clinic-popup__phone-number">{clinic.phone}</p>
-        )}
+        <a
+          href={`tel:${clinic.phone}`}
+          className={`clinic-popup__phone-number, ${getIsPhone() ? 'clinic-popup__phone-number_active' : ''}`}
+          style={{ pointerEvents: getIsPhone() ? 'auto' : 'none' }}
+        >
+          {clinic.phone}
+        </a>
       </div>
       <div className="clinic-popup__site">
         <p className="clinic-popup__site-title">Сайт: </p>
@@ -55,3 +54,39 @@ export function FullCardClinic({ isClose, clinic }: IFullCard) {
     </div>
   );
 }
+
+// function getIsPhone() {
+//   const width = window.screen.width;
+//   if (width < 625) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+// return (
+//   <div className="clinic-popup">
+//     <CloseButton type="button" onClick={isClose} />
+//     <h3 className="clinic-popup__name">{clinic.short_name}</h3>
+//     <p className="clinic-popup__about">{clinic.about}</p>
+//     <div className="clinic-popup__timetable">
+//       <p className="clinic-popup__timetable-title">График работы:</p>
+
+//       {/* TODO@: заменить, когда появится функция парсер */}
+//       <p className="clinic-popup__timetable-period">{'Пн-Пт: 8:00–17:00 Сб-Вс: Выходной'}</p>
+//       {/* <p className="clinic-popup__timetable-period">{clinic.timetable}</p> */}
+//     </div>
+//     <div className="clinic-popup__address">
+//       <p className="clinic-popup__address-text">
+//         <span className="clinic-popup__address-title">Адрес:</span>
+//         {clinic.factual_address}
+//       </p>
+//     </div>
+//     <div className="clinic-popup__phone">
+//       <p className="clinic-popup__phone-title">Телефон: </p>
+//       <a
+//         href={`tel:${clinic.phone}`}
+//         className={`clinic-popup__phone-number, ${getIsPhone() ? 'clinic-popup__phone-number_active' : ''}`}
+//       >
+//         {clinic.phone}
+//       </a>
+//     </div>
