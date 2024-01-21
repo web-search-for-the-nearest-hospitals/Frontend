95caf9e0b7b563dc9972f78fc45fcf023646572c
DariaBold <dafkajh@gmail.com>
2024-01-21 23:01:03 +0300

    refactor(fullcardclinic): удаление комментария

--- src/entities/clinic/ui/FullCardClinic/FullCardClinic.tsx
+++ src/entities/clinic/ui/FullCardClinic/FullCardClinic.tsx
@@ -54,39 +54,3 @@ export function FullCardClinic({ isClose, clinic }: IFullCard) {
     </div>
   );
 }
-
-// function getIsPhone() {
-//   const width = window.screen.width;
-//   if (width < 625) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-// return (
-//   <div className="clinic-popup">
-//     <CloseButton type="button" onClick={isClose} />
-//     <h3 className="clinic-popup__name">{clinic.short_name}</h3>
-//     <p className="clinic-popup__about">{clinic.about}</p>
-//     <div className="clinic-popup__timetable">
-//       <p className="clinic-popup__timetable-title">График работы:</p>
-
-//       {/* TODO@: заменить, когда появится функция парсер */}
-//       <p className="clinic-popup__timetable-period">{'Пн-Пт: 8:00–17:00 Сб-Вс: Выходной'}</p>
-//       {/* <p className="clinic-popup__timetable-period">{clinic.timetable}</p> */}
-//     </div>
-//     <div className="clinic-popup__address">
-//       <p className="clinic-popup__address-text">
-//         <span className="clinic-popup__address-title">Адрес:</span>
-//         {clinic.factual_address}
-//       </p>
-//     </div>
-//     <div className="clinic-popup__phone">
-//       <p className="clinic-popup__phone-title">Телефон: </p>
-//       <a
-//         href={`tel:${clinic.phone}`}
-//         className={`clinic-popup__phone-number, ${getIsPhone() ? 'clinic-popup__phone-number_active' : ''}`}
-//       >
-//         {clinic.phone}
-//       </a>
-//     </div>
