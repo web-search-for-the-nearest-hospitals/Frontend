18cb79413e413690d425e2b74d42725d2e6704cd
Dmitry <rdevil1337@gmail.com>
2024-01-26 19:11:15 +0500

    feat: Timetable in popup (#97)
    * feat: расписание вынес в компонент TimetableList.tsx и прокинул в попап
    
    * refactor: вернул timetablelist в entities
    
    * refactor: изменить директории
    
    ---------
    
    Co-authored-by: Vera Baldina~ <vera.b97@mail.ru>

--- src/entities/clinic/ui/ClinicCard/ClinicCard.tsx
+++ src/entities/clinic/ui/ClinicCard/ClinicCard.tsx
@@ -1,6 +1,7 @@
 import './ClinicCard.scss';
+
 import { IOrganization } from '~/shared/lib/types/interfaces';
-import { getTimetable } from '../../lib/getTimetable';
+import TimetableClinic from '../TimetableClinic/TimetableClinic';
 
 interface IClinicCard {
   clinic: IOrganization;
@@ -8,21 +9,12 @@ interface IClinicCard {
 }
 
 export function ClinicCard({ clinic, handleCardClick }: IClinicCard) {
-  const date = new Date();
-  const today = date.getDay() || 7; // пн = 1, вс = 7
-
   return (
     <div className="clinic-card" onClick={() => handleCardClick(clinic)}>
       <h3 className="clinic-card__name">{clinic.short_name}</h3>
       <div className="clinic-card__timetable">
         <p className="clinic-card__timetable-title">График работы:</p>
-        <ul className="clinic-card__timetable-period">
-          {getTimetable(clinic).map((day, index) => (
-            <li key={index} style={{ color: index + 1 === today ? '#695feb' : '#3b405d' }}>
-              {day}
-            </li>
-          ))}
-        </ul>
+        {TimetableClinic(clinic, 'card')}
       </div>
       <div className="clinic-card__phone">
         <p className="clinic-card__phone-title">Телефон: </p>
