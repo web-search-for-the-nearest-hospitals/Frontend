6de77a44389c20839f8e15eab2c99f1f57c95e4f
rdevil23 <rdevil1337@gmail.com>
2024-01-30 22:45:02 +0500

    refactor: избавился от пустого списка, изменил положение иконки, класс в img в зависимости от стейта

--- src/shared/assets/index.ts
+++ src/shared/assets/index.ts
@@ -1,7 +1,5 @@
 import LocationIcon from './icons/LocationIcon';
 import UserIcon from './icons/UserIcon';
 import BtnCloseIcon from './icons/BtnCloseIcon';
-import ClinicDownBtn from './icons/ClinicDownBtn';
-import ClinicUpBtn from './icons/ClinicUpBtn';
 
-export { LocationIcon, UserIcon, BtnCloseIcon, ClinicDownBtn, ClinicUpBtn };
+export { LocationIcon, UserIcon, BtnCloseIcon };
