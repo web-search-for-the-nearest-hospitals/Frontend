91cc26b723c1434177f55200b5c3e6d9aff4fba1
Magomed Musakadiev <musakadiev-mag@yandex.ru>
2024-01-31 23:20:22 +0300

    fix: адаптировал под мобилку

--- src/features/appointment-form/ui/index.tsx
+++ src/features/appointment-form/ui/index.tsx
@@ -51,97 +51,99 @@ export default function AppointmentForm() {
   };
 
   return (
-    <div className="appointment-page">
-      {formCh === 2 ? (
-        <button className="appointment-page__button-back" onClick={() => setFormCh(1)}>
-          <span className="appointment-page__arrow-back"> &#8249;</span> Назад
-        </button>
-      ) : null}
-      <form className="form-appointment">
-        <h3 className="form-appointment__title">Запись на приём</h3>
-        {formCh === 1 ? (
-          <section className="form-appointment__ch1">
-            <div className="form-appointment__drop-down-container">
-              <DropDownInput
-                values={specialties.map((obj) => obj.skill)}
-                placeholder="Врач, специальность"
-                state={specialty}
-                setState={setSpecialty}
-                isContentEditable
-              />
-            </div>
-            <div className="form-appointment__calendar-container">
-              <Calendar setDate={setDate} />
-            </div>
-            {queryResult.isLoading ? <p>Загружаю доступные ячейки записи</p> : null}
-            {queryResult.isError ? <p>Так далеко в будущее мы не заглядываем</p> : null}
-            {queryResult.currentData?.length === 0 && !queryResult.isError ? <p>Похоже талонов нет...</p> : null}
-            {queryResult.currentData?.length ? <Coupons couponsData={queryResult.currentData} /> : null}
-            <div className="form-appointment__button-container">
-              <Button
-                title="Далее"
-                type="button"
-                size="forForm"
-                disabled={queryResult.isError || queryResult.currentData?.length === 0}
-                onClick={() => setFormCh(2)}
-              />
-            </div>
-          </section>
-        ) : null}
-
+    <>
+      <div className="appointment-page">
         {formCh === 2 ? (
-          <section className="form-appointment__ch2">
-            <InputForm
-              type={'text'}
-              name={'surname'}
-              title={'Фамилия'}
-              placeholder={'Иванов'}
-              minLength={2}
-              maxLength={27}
-              pattern={regex.name}
-            />
-            <InputForm
-              type={'text'}
-              name={'name'}
-              title={'Имя'}
-              placeholder={'Иван'}
-              minLength={2}
-              maxLength={27}
-              pattern={regex.name}
-            />
-
-            <InputForm
-              type={'text'}
-              name={'fatherName'}
-              title={'Отчество'}
-              placeholder={'Иванович'}
-              minLength={2}
-              maxLength={27}
-              pattern={regex.name}
-            />
-            <InputForm
-              type={'tel'}
-              name={'tel'}
-              title={'Номер телефона'}
-              placeholder={'+7 (900) 809 00 50'}
-              // minLength={5}
-              // maxLength={16}
-              // pattern={regex.tel}
-            />
-
-            <label className="form-appointment__consent" onClick={handleCheckboxConsent}>
-              <Checkbox state={stateCheckbox} setState={setStateCheckbox} title={''} />
-              <p className="form-appointment__consent-text">
-                Я соглашаюсь с условиями использования сайта и даю согласие на обработку своих персональных данных в
-                соответствии сполитикой обработки персональных данных.
-              </p>
-            </label>
-            <div className="form-appointment__button-container">
-              <Button title="Записаться" size="forForm" disabled={buttonSubmit} />
-            </div>
-          </section>
+          <button className="appointment-page__button-back" onClick={() => setFormCh(1)}>
+            <span className="appointment-page__arrow-back"> &#8249;</span> Назад
+          </button>
         ) : null}
-      </form>
-    </div>
+        <form className="form-appointment">
+          <h3 className="form-appointment__title">Запись на приём</h3>
+          {formCh === 1 ? (
+            <section className="form-appointment__ch1">
+              <div className="form-appointment__drop-down-container">
+                <DropDownInput
+                  values={specialties.map((obj) => obj.skill)}
+                  placeholder="Врач, специальность"
+                  state={specialty}
+                  setState={setSpecialty}
+                  isContentEditable
+                />
+              </div>
+              <div className="form-appointment__calendar-container">
+                <Calendar setDate={setDate} />
+              </div>
+              {queryResult.isLoading ? <p>Загружаю доступные ячейки записи</p> : null}
+              {queryResult.isError ? <p>Так далеко в будущее мы не заглядываем</p> : null}
+              {queryResult.currentData?.length === 0 && !queryResult.isError ? <p>Похоже талонов нет...</p> : null}
+              {queryResult.currentData?.length ? <Coupons couponsData={queryResult.currentData} /> : null}
+              <div className="form-appointment__button-container">
+                <Button
+                  title="Далее"
+                  type="button"
+                  size="forForm"
+                  disabled={queryResult.isError || queryResult.currentData?.length === 0}
+                  onClick={() => setFormCh(2)}
+                />
+              </div>
+            </section>
+          ) : null}
+
+          {formCh === 2 ? (
+            <section className="form-appointment__ch2">
+              <InputForm
+                type={'text'}
+                name={'surname'}
+                title={'Фамилия'}
+                placeholder={'Иванов'}
+                minLength={2}
+                maxLength={27}
+                pattern={regex.name}
+              />
+              <InputForm
+                type={'text'}
+                name={'name'}
+                title={'Имя'}
+                placeholder={'Иван'}
+                minLength={2}
+                maxLength={27}
+                pattern={regex.name}
+              />
+
+              <InputForm
+                type={'text'}
+                name={'fatherName'}
+                title={'Отчество'}
+                placeholder={'Иванович'}
+                minLength={2}
+                maxLength={27}
+                pattern={regex.name}
+              />
+              <InputForm
+                type={'tel'}
+                name={'tel'}
+                title={'Номер телефона'}
+                placeholder={'+7 (900) 809 00 50'}
+                // minLength={5}
+                // maxLength={16}
+                // pattern={regex.tel}
+              />
+
+              <label className="form-appointment__consent" onClick={handleCheckboxConsent}>
+                <Checkbox state={stateCheckbox} setState={setStateCheckbox} title={''} />
+                <p className="form-appointment__consent-text">
+                  Я соглашаюсь с условиями использования сайта и даю согласие на обработку своих персональных данных в
+                  соответствии сполитикой обработки персональных данных.
+                </p>
+              </label>
+              <div className="form-appointment__button-container">
+                <Button title="Записаться" size="forForm" disabled={buttonSubmit} />
+              </div>
+            </section>
+          ) : null}
+        </form>
+      </div>
+    </>
   );
 }
