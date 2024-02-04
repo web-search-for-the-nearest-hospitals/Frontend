9ac0319b1c0ae33f9e30ec7020d8ce472a72d2e5
rdevil23 <rdevil1337@gmail.com>
2024-02-04 18:01:40 +0500

    style: добавил анимацию + небольшой рефактор

--- src/entities/clinic/ui/TimetableClinic/TimetableClinic.tsx
+++ src/entities/clinic/ui/TimetableClinic/TimetableClinic.tsx
@@ -1,29 +1,39 @@
+import cn from 'classnames';
+
+import ico from '~/shared/assets/icons/timetable-arrow-up.svg';
+
+import { useState } from 'react';
+
 import { IOrganization } from '~/shared/lib/types/interfaces';
 import { getTimetable } from '../../lib/getTimetable';
-import { useState } from 'react';
-import ico from '~/shared/assets/icons/timetable-arrow-up.svg';
 
 export default function TimetableClinic(clinic: IOrganization, name: string) {
   const date = new Date();
   const today = date.getDay() || 7;
 
-  const [isFullTimetable, setFullTimetable] = useState(false);
-  const handleFullTimetable = () => {
-    setFullTimetable(!isFullTimetable);
+  const [isOpen, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(!isOpen);
   };
 
   const whatToShow = (day: string, index: number) => {
     if (index + 1 === today && day.includes('Выходной') === true) {
+      /** Что приходит в day: 'Сб: Выходной'
+       * Что получаем после day.slice(4): 'Выходной'
+       */
       return `${day.slice(4)}`;
     }
+    /** Что приходит в day: 'Пн: 10:00-20:00'
+     * Что получаем после day.slice(-5): '20:00'
+     */
     return index + 1 === today && `Открыто до: ${day.slice(-5)}`;
   };
 
   return (
-    <div onClick={handleFullTimetable} className={`clinic-${name}__timetable-period`}>
-      <ul style={{ color: '#695feb' }}>
+    <div onClick={handleOpen} className={`clinic-${name}__timetable-period`}>
+      <ul className={cn(`clinic-${name}__timetable-list`, isOpen && `clinic-${name}__timetable-list_open`)}>
         {getTimetable(clinic).map((day, index) =>
-          isFullTimetable ? (
+          isOpen ? (
             <li key={index} style={{ color: index + 1 === today ? '#695feb' : '#3b405d' }}>
               {day}
             </li>
@@ -35,11 +45,7 @@ export default function TimetableClinic(clinic: IOrganization, name: string) {
       <img
         src={ico}
         alt="dropdown icon"
-        className={
-          isFullTimetable
-            ? `clinic-${name}__timetable-ico clinic-${name}__timetable-ico_up`
-            : `clinic-${name}__timetable-ico`
-        }
+        className={cn(`clinic-${name}__timetable-ico`, isOpen && `clinic-${name}__timetable-ico_up`)}
       />
     </div>
   );
